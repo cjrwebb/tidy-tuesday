90f3ca52036a2cddd8ebd634ed1a6f9e398737f0
cjrwebb <66517368+cjrwebb@users.noreply.github.com>
2021-03-23 12:11:00 +0000

    Tidied up

--- 2021-03-23-unvotes/script.R
+++ 2021-03-23-unvotes/script.R
@@ -1,10 +1,6 @@
 library(tidyverse)
 library(rnaturalearth)
-library(patchwork)
-library(countrycode)
 library(tmap)
-library(grid)
-library(ggplotify)
 
 unvotes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/unvotes.csv')
 roll_calls <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/roll_calls.csv')
